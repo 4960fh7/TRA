410e124a4b46904d4a3d58a3d364ce7024953e2b
4960fh7 <roy0chu@gmail.com>
2026-08-20 17:05:36 +0800

    fix style

--- history.js
+++ history.js
@@ -320,7 +320,8 @@ function renderTrainCharts() {
 
         let isMobile = window.innerWidth <= 600;
         let yMax = isMobile ? Math.max(10, localMaxDelay * 1.1) : window.yAxisMax;
-        let mobileHeight = Math.max(70, ((localMaxDelay / (window.yAxisMax || 1)) * 200 + 40) * 0.4);
+        
+        let mobileHeight = isMobile ? Math.floor(130 + (localMaxDelay / (window.yAxisMax || 1)) * 70) : 400;
 
         const containerHTML = `
             <div class="chart-container" id="chart-train-${train.No}" style="overflow: hidden; --mobile-height: ${mobileHeight}px;">
@@ -673,7 +674,8 @@ function renderStationCharts() {
 
         let isMobile = window.innerWidth <= 600;
         let yMax = isMobile ? Math.max(10, localMaxDelay * 1.1) : window.yAxisMax;
-        let mobileHeight = Math.max(70, ((localMaxDelay / (window.yAxisMax || 1)) * 200 + 40) * 0.4);
+        
+        let mobileHeight = isMobile ? Math.floor(130 + (localMaxDelay / (window.yAxisMax || 1)) * 70) : 400;
 
         const containerHTML = `
             <div class="chart-container" id="chart-station-${sid}" style="overflow: hidden; --mobile-height: ${mobileHeight}px;" data-sname="${sName}" data-sid="${sid}" data-ymax="${yMax}">
